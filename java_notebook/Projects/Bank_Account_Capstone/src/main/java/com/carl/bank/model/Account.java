da32f7a6b06189043360ebd15853dd9e73efddd5
CarlBorba <business10carlos@gmail.com>
2026-09-11 07:25:21 -0300

    feat(bank): implement Account model with equals and hashCode contracts

---
+++ java_notebook/Projects/Bank_Account_Capstone/src/main/java/com/carl/bank/model/Account.java
@@ -0,0 +1,50 @@
+package com.carl.bank.model;
+
+import java.util.Objects;
+
+public class Account {
+    private final int id;
+    private String holderName;
+    private double balance;
+
+    // CONSTRUCTOR
+    public Account(int id, String holderName, double balance) {
+        this.id = id;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // GETTERS & SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id == account.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
